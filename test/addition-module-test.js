6ca8acd949dedb5dd9fe280050d0fb72684e5395
Adam Prendergast <prenders82@yahoo.com>
2015-01-02 16:29:45 +0000

    Update addition-module-test.js

--- test/addition-module-test.js
+++ test/addition-module-test.js
@@ -9,7 +9,7 @@ describe("Calculator Module", function() {
     it('should return 4 when adding 2 and 2', function(){
       assert.equal(4, calculator.add(2, 2));
     });
-    it('should return 2 when passes 1 and 1 as string values', function(){
+    it('should return 2 when passed 1 and 1 as string values', function(){
       assert.equal(2, calculator.add('1', '1'));
     });
 	});
